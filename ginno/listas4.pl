73a7271b59d4ff2273523cca222609757b5a1946
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2025-05-20 14:21:34 -0400

    add new methods

--- ginno/listas4.pl
+++ ginno/listas4.pl
@@ -1,4 +1,4 @@
-% reemplaza un elemento A, por otro B
+% 1. reemplaza un elemento A, por otro B
 replace([], _, _, []).
 
 replace([X|L], A, B, Result) :-
@@ -10,7 +10,7 @@ replace([X|L], A, B, Result) :-
   replace(L, A, B, R),
   Result = [X|R].
 
-% obtenerPrim(n) devuelve la lista con los primeros n elmentos
+% 2. obtenerPrim(n) devuelve la lista con los primeros n elmentos
 obtenerPrim(_, 0, []).
 
 obtenerPrim([X|L], N, Result) :-
@@ -18,7 +18,7 @@ obtenerPrim([X|L], N, Result) :-
   obtenerPrim(L, N1, R),
   Result = [X|R].
 
-% obtenerUlt(n) devuelve la lista con los primeros n elmentos
+% 3. obtenerUlt(n) devuelve la lista con los ultimos n elmentos
 obtenerUlt(_, 0, []). % caso base 1
 
 obtenerUlt([X|L], N, Result) :- % caso base 2
@@ -28,4 +28,19 @@ obtenerUlt([X|L], N, Result) :- % caso base 2
 
 obtenerUlt([_|L], N, Result) :-
   obtenerUlt(L, N, R),
+  Result = R.
+
+% 4. eliminarPrim(n) elimina los primeros n elementos 
+eliminarPrim(L, 0, L).
+
+eliminarPrim([_|L], N, Result) :-
+  N1 is N - 1,
+  eliminarPrim(L, N1, R),
+  Result = R.
+
+% 5. eliminarUlt(N) elimina los ultimos n elementos 
+eliminarUlt(L, N, Result) :-
+  length(L, Length),
+  N1 is Length - N,
+  obtenerPrim(L, N1, R),
   Result = R.
